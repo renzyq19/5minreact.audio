8200377ebc396c0adb21f5ec674315e5b64ccfc8
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-09-03 19:05:03 +0100

    initial commit

---
+++ src/components/SiteLinks/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {RouteHandler} from "react-router";
+import {prefixLink} from "gatsby-helpers";
+import {config} from "config";
+import "./style.css";
+
+class SiteLinks extends React.Component {
+    render() {
+
+        return (
+            <div className="sitelinks">
+                <ul>
+                    <li><a href={ config.siteItunesUrl } target="_blank">iTunes</a></li>
+                    <li><a href={ config.siteAndroidUrl } title="Subscribe on Android">Subscribe on Android</a></li>
+                    <li><a href={ config.siteSoundcloudUrl } target="_blank">SoundCloud</a></li>
+                    <li><a href={ config.siteRssUrl } target="_blank">RSS</a></li>
+                    <li><a href={ config.siteTwitterUrl } target="_blank">Twitter</a></li>
+                    <li><a href={ config.siteYouTubeUrl } target="_blank">YouTube</a></li>
+                    <li><a href={ config.siteEmailUrl }>Email</a></li>
+                </ul>
+
+                <ul>
+                    <li><small><a href="http://opencollective.com/webpack" target="_blank">Silver sponsor of Webpack</a></small></li>
+                    <li><small><a href="http://5minreact.ru" target="_blank">Listen in Russian</a></small></li>
+                    <li><small>Author: <a href="https://twitter.com/petrmyazin" target="_blank">@PetrMyazin</a></small></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default SiteLinks

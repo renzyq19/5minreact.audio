b5730221cc52b4cce7a4fb7cce21952640940baa
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-09-06 11:13:07 +0100

    linted index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,61 +11,58 @@ import './index.css'
 const linksAtATime = 2
 
 class SiteIndex extends React.Component {
-  constructor(props){
+  constructor (props) {
     super(props)
-    let linkstoshow = linksAtATime
-
     this.state = {
-      linkstoshow
+      linkstoshow: linksAtATime,
     }
   }
-    render() {
-      console.log(this.props.data)
-      console.log(this.state.linkstoshow)
-        const sortedPages = this.props.data.allMarkdownRemark.edges
-        const siteData = this.props.data.site.siteMetadata
+  render () {
+    console.log(this.props.data)
+    console.log(this.state.linkstoshow)
+    const sortedPages = this.props.data.allMarkdownRemark.edges
+    const siteData = this.props.data.site.siteMetadata
 
-        const pageLink =  ({ node }) => {
-                const title = node.frontmatter.title
-                const body = node.html.split('<hr>')[0]
-                const datePublished = node.frontmatter.date
-              
-                // 
-                // Wasn't sure what this was doing, and it was already commented out, so I've left it here
-                //
-                //    const category = access(page, 'data.category')
-                //    <span className='blog-category'>{ category }</span>
+    const pageLink = ({ node }) => {
+      const title = node.frontmatter.title
+      const body = node.html.split('<hr>')[0]
+      const datePublished = node.frontmatter.date
+      //
+      // Wasn't sure what this was doing, and it was already commented out, so I've left it here
+      //
+      //    const category = access(page, 'data.category')
+      //    <span className='blog-category'>{ category }</span>
 
-                return (
-                    <div className='blog-post' key={title}>
-                      <h2><Link style={ {    borderBottom: 'none',} } to={ node.fields.slug } > { title } </Link></h2>
-                      <time dateTime={ moment(datePublished).format('MMMM D, YYYY') }>
-                        { moment(datePublished).format('DD MMMM YYYY') }
-                      </time>
-                      <p dangerouslySetInnerHTML={{ __html: body}} />
-                    </div> )
-        }
-
-        return (
-            <DocumentTitle title={ siteData.title }>
-              <div>
-                <SiteSidebar isHome={true} siteData={siteData}/>
-                <div className='content'>
-                  <div className='main'>
-                    <div className='main-inner'>
-                      { sortedPages.slice(0,this.state.linkstoshow).map(pageLink) }
-                      { this.state.linkstoshow < sortedPages.length ? (
-                        <div className="loadmore" onClick={() => this.setState(prev => ({linkstoshow: prev.linkstoshow + linksAtATime}))} >
-                          Load More
-                        </div>
-                      ) : <div />}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </DocumentTitle>
-        )
+      return (
+        <div className="blog-post" key={title}>
+          <h2><Link style={{ borderBottom: 'none' }} to={node.fields.slug}> { title } </Link></h2>
+          <time dateTime={moment(datePublished).format('MMMM D, YYYY')}>
+            { moment(datePublished).format('DD MMMM YYYY') }
+          </time>
+          <p dangerouslySetInnerHTML={{ __html: body }} />
+        </div>)
     }
+
+    return (
+      <DocumentTitle title={siteData.title}>
+        <div>
+          <SiteSidebar isHome siteData={siteData}/>
+          <div className="content">
+            <div className="main">
+              <div className="main-inner">
+                { sortedPages.slice(0, this.state.linkstoshow).map(pageLink) }
+                { this.state.linkstoshow < sortedPages.length ? (
+                  <div className="loadmore" onClick={() => this.setState(prev => ({linkstoshow: prev.linkstoshow + linksAtATime}))} >
+                    Load More
+                  </div>
+                ) : <div />}
+              </div>
+            </div>
+          </div>
+        </div>
+      </DocumentTitle>
+    )
+  }
 }
 
 SiteIndex.propTypes = {
